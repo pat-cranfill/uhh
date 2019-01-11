a8a596fd394cbb507f3c6368b3b3d588011525e6
Patrick Cranfill <pat.cranfill@gmail.com>
2019-01-10 22:59:39 -0500

    design changes to banner & nav w/ a bit more JS

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-$('#about-card').scroll(function () {
-    $('#about-card').animate({top: '-=20'}, 'slow');
-    $('#about-card').animate({top: '+=20'}, 'slow');
+$(document).ready(() => {
+    $('.navbar').hide();
+    $('.jumbo').hide();
+    $('.navbar').slideDown(1000);
+    $('.jumbo').slideDown(1000);
+});
+
+$('.nav-link').hover(function(){
+    $(this).css("border-bottom", "solid 2px grey").delay(2000);
+}, function () {
+    $(this).css("border", "0");
 });
